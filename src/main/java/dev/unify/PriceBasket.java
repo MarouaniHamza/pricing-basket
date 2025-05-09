65efb21d1d9cdda857309ebcfbc6240a6ed6dd2a
MarouaniHamza <hamza.marouenihamza@gmail.com>
2025-05-09 11:08:42 +0200

    feat(dev): Add Main class

--- src/main/java/dev/unify/PriceBasket.java
+++ src/main/java/dev/unify/PriceBasket.java
@@ -1,9 +1,35 @@
 package dev.unify;
 
 
+import dev.unify.model.Item;
+import dev.unify.service.impl.ConditionalDiscountOffer;
+import dev.unify.service.impl.ConsolePurchaseSummaryPrinter;
+import dev.unify.service.impl.PurchaseSummaryServiceImpl;
+import dev.unify.service.impl.SimpleDiscountOffer;
+import dev.unify.utils.BasketUtils;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceBasket {
 
     public static void main(String[] args) {
-
+        final var appleTenPercentDiscountOffer = SimpleDiscountOffer.builder()
+                .label("Apples 10% off")
+                .appliedTo(Item.APPLES)
+                .percentage(BigDecimal.valueOf(0.1))
+                .build();
+        final var halfBreadForTwoSoupDiscountOffer = ConditionalDiscountOffer.builder()
+                .label("2 tins of soup and get a loaf of bread")
+                .appliedTo(Item.BREAD)
+                .percentage(BigDecimal.valueOf(0.5))
+                .baseItem(Item.SOUP)
+                .quantity(2)
+                .build();
+        final var purchaseSummary = new PurchaseSummaryServiceImpl().getPurchaseSummary(
+                BasketUtils.fromList(List.of(args)),
+                List.of(appleTenPercentDiscountOffer, halfBreadForTwoSoupDiscountOffer)
+        );
+        new ConsolePurchaseSummaryPrinter().print(purchaseSummary);
     }
 }
